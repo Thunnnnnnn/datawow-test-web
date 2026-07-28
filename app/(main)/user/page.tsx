eb4ea8583fff04b805ba3d6f6a132698f8996de3
Thun Wityakon <thunwi@hotmail.com>
2026-07-28 23:54:07 +0700

    Connect api in /user

--- app/(main)/user/page.tsx
+++ app/(main)/user/page.tsx
@@ -2,22 +2,53 @@
 import styles from './UserPage.module.css';
 import { User } from 'lucide-react';
 import { useConcertStore } from "@/store/concertStore";
+import { useBookHistoryStore } from "@/store/bookHistoryStore";
 import { useEffect } from "react";
 
 export default function UserPage() {
     const store = useConcertStore();
-
+    const bookHistoryStore = useBookHistoryStore();
+    const fetchData = async () => {
+        try {
+            await store.getConcerts();
+            await bookHistoryStore.getBookHistoriesByUser();
+        } catch (error) {
+            console.error("Error fetching concerts:", error);
+        }
+    };
     useEffect(() => {
-        const fetchConcerts = async () => {
-            try {
-                await store.getConcerts();
-            } catch (error) {
-                console.error("Error fetching concerts:", error);
-            }
-        };
-
-        fetchConcerts();
+        fetchData();
     }, []);
+
+    const reserveConcert = (concertId: number) => async () => {
+        const find = bookHistoryStore.bookHistories.find((history) => history.concertId === concertId);
+        if (find) {
+            const res = await bookHistoryStore.updateBookHistoryStatus(find.id, 'RESERVE');
+            if (res.status) {
+                alert('Concert reserved successfully!');
+                await fetchData();
+            }
+            return;
+        } else {
+            const res = await bookHistoryStore.createBookHistory(concertId);
+    
+            if (res.status) {
+                alert('Concert reserved successfully!');
+                await fetchData();
+            }
+        }
+    }
+
+    const cancelReservation = (concertId: number) => async () => {
+        const bookHistory = bookHistoryStore.bookHistories.find((history) => history.concertId === concertId);
+        if (bookHistory) {
+            const res = await bookHistoryStore.updateBookHistoryStatus(bookHistory.id, 'CANCEL');
+            if (res.status) {
+                alert('Reservation canceled successfully!');
+                await fetchData();
+            }
+        }
+    }
 
     return (
         <div className="flex flex-col gap-4">
@@ -36,18 +67,21 @@ export default function UserPage() {
                             </span>
                         </div>
                         <div className="flex gap-2">
-                            <button className={styles['save-btn']}>
-                                Reserve
-                            </button>
-                            <button className={styles['delete-btn']}>
-                                Cancel
-                            </button>
+                            {
+                                bookHistoryStore.bookHistories.find((history) => history.concertId === concert.id)?.status === 'RESERVE' ?
+                                    <button className={styles['delete-btn']} onClick={cancelReservation(concert.id)}>
+                                        Cancel
+                                    </button> :
+                                    <button className={styles['save-btn']} onClick={reserveConcert(concert.id)}>
+                                        Reserve
+                                    </button>
+                            }
                         </div>
                     </div>
                 </div>
-            ))}
-
-        </div>
+            ))
+            }
+        </div >
 
     )
 }
